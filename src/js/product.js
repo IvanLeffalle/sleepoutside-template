996798af192790bed798a40b6d6194d3d4baeb4a
IvanLeffalle <ivanmleffalle@gmail.com>
2025-01-18 20:12:10 -0300

    week 2- Team Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,8 @@
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-
+import { getParams } from "./utils.mjs";
 const dataSource = new ProductData("tents");
+import ProductDetails from "./ProductDetails.mjs";
 
 function addProductToCart(product) {
   // add product to cart
@@ -20,3 +21,27 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+const productID = getParams("product");
+console.log("Product ID from URL:", productID);
+
+async function renderProduct() {
+  try {
+    const product = await dataSource.findProductById(productID);
+    console.log("Product Data:", product);
+    document.getElementById("brand").textContent = product.Brand.Name;
+    document.getElementById("name").textContent = product.NameWithoutBrand;
+    document.getElementById("productImage").src = product.Image;
+    document.getElementById("price").textContent = `$${product.FinalPrice}`;
+    document.getElementById("color").textContent = product.Colors.ColorName;
+    document.getElementById("description").innerHTML =
+      product.DescriptionHtmlSimple;
+  } catch (error) {
+    console.error("Error fetching product:", error);
+  }
+}
+
+renderProduct();
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
